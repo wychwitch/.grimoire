1fb128317d3ec175970c6ff24173c2c13565328a
wych(witch) <wychwitchcraft@gmail.com>
2022-05-25 20:57:32 +0000

    add missing symlink commands

--- summon.sh
+++ summon.sh
@@ -2,6 +2,13 @@
 #!/bin/bash
 
 ## all the symbolic links
+echo "Linking gitconfig"
 ln -s ~/.grimoire/.gitconfig ~/.gitconfig
+echo "Linking zshrc "
 ln -s ~/.grimoire/.zshrc ~/.zshrc
+echo "Linking ssh_config"
 ln -s ~/.grimoire/ssh_config ~/.ssh/config
+echo "Linking posh-themes "
+ln -s ~/.grimoire/.posh-themes ~/.posh-themes
+echo "Linking li commmand "
+sudo ln -s ~/.grimoire/commands/li /usr/local/bin/li
