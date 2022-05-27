d59a9516f208fa29b1d8982337940091b2d77f51
wych(witch) <wychwitchcraft@gmail.com>
2022-05-27 12:53:22 -0500

    adjusted summon script

---
+++ fresh-summon.sh
@@ -0,0 +1,5 @@
+sudo apt-get update
+sudo apt-get install zsh vim ssh python3 python3-pip
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew Brewfile
+./summon.sh
--- summon.sh
+++ summon.sh
@@ -3,5 +3,8 @@
 
 ## install dotdrop
 sudo apt-get install python3-pip
+git submodule update --remote
+./dotdrop
 pip3 install --user -r dotdrop/requirements.txt
+./dotdrop
 sudo chmod 777 -R .git/modules/dotdrop/
