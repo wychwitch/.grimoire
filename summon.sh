9429d850bff8fffb50af74c58aea11a5bd9b5b9c
wych(witch) <wychwitchcraft@gmail.com>
2022-05-27 17:14:30 +0000

    upd8

--- summon.sh
+++ summon.sh
@@ -1,14 +1,7 @@
 
 #!/bin/bash
 
-## all the symbolic links
-echo "Linking gitconfig"
-ln -s ~/.grimoire/.gitconfig ~/.gitconfig
-echo "Linking zshrc "
-ln -s ~/.grimoire/.zshrc ~/.zshrc
-echo "Linking ssh_config"
-ln -s ~/.grimoire/ssh_config ~/.ssh/config
-echo "Linking posh-themes "
-ln -s ~/.grimoire/.posh-themes ~/.posh-themes
-echo "Linking li commmand "
-sudo ln -s ~/.grimoire/commands/li /usr/local/bin/li
+## install dotdrop
+sudo apt-get install python3-pip
+pip3 install --user -r dotdrop/requirements.txt
+sudo chmod 777 -R .git/modules/dotdrop/
--- zshrc
+++ zshrc
@@ -106,7 +106,7 @@ eval "$(ssh-agent -s)"
 export PATH="$HOME/.local/bin:$PATH"
 
 export LD_LIBRARY_PATH=/home/linuxbrew/.linuxbrew/lib:$LD_LIBRARY_PATH
-
+[ -f .aliases ] && source .aliases
 NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
