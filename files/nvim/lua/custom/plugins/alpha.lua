5912f6db985a0ca069703731031c7f4a1831618d
wych(witch) <wych@wychwit.ch>
2023-03-16 21:26:04 -0500

    smole change

--- files/nvim/lua/custom/plugins/alpha.lua
+++ files/nvim/lua/custom/plugins/alpha.lua
@@ -1,6 +1,37 @@
 return {
 	'goolord/alpha-nvim',
 	config = function()
-		require 'alpha'.setup(require 'alpha.themes.dashboard'.config)
+		local alpha = require 'alpha'
+		local dashboard = require 'alpha.themes.dashboard'
+		dashboard.section.header.val = {
+			[[  ▄██▄       ▄▄▄██████▄                         ▄██████▄▄▄       ▄██▄  ]],
+			[[▐██▌       ▄██████████████                   ██████████████▄       ▐██▌]],
+			[[▐███▄▄▄▄███████████████████                 ███████████████████▄▄▄▄███▌]],
+			[[ ▀██████████▀▀▀▀▀▀██████████               ██████████▀▀▀▀▀▀██████████▀ ]],
+			[[▀█▄  ▀▀     ▄▄▄▄▄▄  ▀███████▌             ████████▀  ▄▄▄▄▄▄     ▀▀  ▄█▀]],
+			[[ ▀██████████████████  ███████             ███████  ██████████████████▀ ]],
+			[[     ▀▀▀       ▀█████ ███████▌           ▐███████ █████▀       ▀▀▀     ]],
+			[[        ▄▄▄▄▄    ████ ▐███████▄         ▄███████▌ ████    ▄▄▄▄▄        ]],
+			[[      ▄████████  ▐███▌ ████████▄       ▄████████ ████▌  ████████▄      ]],
+			[[      ██▌  ▄ ███  ████▄ ▀████████▄   ▄████████▀ ▄████  ███ ▄  ▐██      ]],
+			[[      ▀██▄▄█▌ ███  ▀████▄ ▀████████ ████████▀ ▄████▀  ███ ▐█▄▄██▀      ]],
+			[[         ▀▀    ███▄▄ ▀▀████▄ ▀▀████ ████▀▀ ▄████▀▀ ▄▄███    ▀▀         ]],
+			[[                ▀█████▄▄▄▄▄ ▄▄ ▄███ ███▄ ▄▄ ▄▄▄▄▄█████▀                ]],
+			[[                   ▀████████▀▄███▀▀ ▀▀███▄▀████████▀                   ]],
+			[[                          ▄██▀▀         ▀▀██▄                          ]],
+			[[                       ▄▄▀▀                 ▀▀▄▄                       ]],
+			[[			       heir o█f light                            ]],
+		}
+		dashboard.section.buttons.val = {
+			dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
+			dashboard.button("q", "  Abscond", ":qa<CR>"),
+		}
+		local handle = io.popen('fortune')
+		local fortune = handle:read("*a")
+		handle:close()
+		dashboard.section.footer.val = fortune
+		dashboard.config.opts.noautocmd = true
+		vim.cmd [[autocmd User AlphaReady echo 'ready']]
+		alpha.setup(dashboard.config)
 	end
 }
--- files/wezterm.lua
+++ files/wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require 'wezterm'
 
 return {
     font = wezterm.font 'Fantasque Sans Mono',
-    font_size = 14.0,
+    font_size = 18.0,
     color_scheme = "Catppuccin Frappe",
     default_prog = { "{{shell}}" },
     default_cwd = "{{directory}}",
