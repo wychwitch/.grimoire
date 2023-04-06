53f0329b0bbdbb0914e7cffee20cee9326a7bd26
wych(witch) <wych@wychwit.ch>
2023-04-06 14:03:40 -0500

    better best zenmode

--- files/nvim/init.lua
+++ files/nvim/init.lua
@@ -207,7 +207,6 @@ require("obsidian").setup({
 require 'mind'.setup()
 -- [[ Setting options ]]
 -- See `:help vim.o`
-
 -- Set highlight on search
 vim.o.hlsearch = true
 
@@ -259,10 +258,11 @@ vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
+local zen = require('zen-mode')
 -- * custom *
 vim.keymap.set('n', '<leader>s', "nil", { desc = "+search" })
 vim.keymap.set('n', '<leader>f', "nil", { desc = "+file" })
-vim.keymap.set('n', '<leader>z', "nil", { desc = "+zen mode" })
+vim.keymap.set('n', '<leader>z', zen.toggle, { desc = "zen mode" })
 
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
@@ -306,10 +306,6 @@ vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc
 vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
 vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
 vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
-local truezen = require('true-zen')
-vim.keymap.set('n', '<leader>zf', truezen.focus, { noremap = true, desc = '[Z]en [F]ocus' })
-vim.keymap.set('n', '<leader>zm', truezen.minimalist, { noremap = true, desc = '[Z]en [M]inimal' })
-vim.keymap.set('n', '<leader>za', truezen.ataraxis, { noremap = true, desc = '[Z]en [A]taraxis' })
 -- [[ Configure Treesitter ]]
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
