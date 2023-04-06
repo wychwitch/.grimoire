0f73ef68beffa0beb10c57b5fe5f66f2d3f1afba
wych(witch) <wych@wychwit.ch>
2023-04-06 16:35:49 -0500

    cmdline

--- files/nvim/init.lua
+++ files/nvim/init.lua
@@ -473,6 +473,20 @@ mason_lspconfig.setup_handlers {
 local cmp = require 'cmp'
 local luasnip = require 'luasnip'
 
+cmp.setup.cmdline(':', {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = cmp.config.sources({
+    { name = 'path' }
+  }, {
+    {
+      name = 'cmdline',
+      option = {
+        ignore_cmds = { 'Man', '!' }
+      }
+    }
+  })
+})
+
 luasnip.config.setup {}
 
 cmp.setup {
