81c010652b94601398f472a529bf33c6d4a96a05
wych(witch) <wych@wychwit.ch>
2023-04-10 09:19:22 -0500

    oh

---
+++ files/emacs/emacs-profiles.el
@@ -0,0 +1,4 @@
+(("default" . ((user-emacs-directory . "C:\\Git\\grimoire\\custom.emacs.d"))))
+("doom" . ((user-emacs-directory . "C:\\Git\\grimoire\\doom.emacs.d")))
+(("spacemacs" . ((user-emacs-directory . "C:\\Git\\grimoire\\spacemacs.emacs.d")))
+(("magic" . ((user-emacs-directory . "C:\\Git\\grimoire\\custom.emacs.d"))))
