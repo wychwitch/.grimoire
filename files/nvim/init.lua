10394640dfaed26ee532c7b19e1a45e824abfcc3
wych(witch) <wych@wychwit.ch>
2023-07-04 15:15:24 -0500

    fixed clipboard issue

--- files/nvim/init.lua
+++ files/nvim/init.lua
@@ -218,7 +218,7 @@ vim.o.mouse = 'a'
 -- Sync clipboard between OS and Neovim.
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
-vim.o.clipboard = {
+vim.g.clipboard = {
   name = "WSL CLIPBOARD",
   copy = {
     ["+"] = 'clip.exe',
